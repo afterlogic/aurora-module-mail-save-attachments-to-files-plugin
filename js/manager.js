ea070ba561fab3d34b594415b85c221a016c3503
nadine-shi <nadinshi@gmail.com>
2022-07-05 11:06:03 +0300

    Improve SelectFilesPopup

--- js/manager.js
+++ js/manager.js
@@ -5,9 +5,7 @@ module.exports = function (appData) {
 		TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
 
 		App = require('%PathToCoreWebclientModule%/js/App.js'),
-		ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js'),
-
-		FilesUtils = require('modules/%ModuleName%/js/utils/Files.js')
+		ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js')
 	;
 
 	if (!ModulesManager.isModuleEnabled('FilesWebclient')) {
@@ -20,8 +18,12 @@ module.exports = function (appData) {
 				App.subscribeEvent('MailWebclient::AddAllAttachmentsDownloadMethod', fAddAllAttachmentsDownloadMethod => {
 					fAddAllAttachmentsDownloadMethod({
 						'Text': TextUtils.i18n('%MODULENAME%/ACTION_SAVE_ATTACHMENTS_TO_FILES'),
-						'Handler': function (accountId, hashes) {
-							FilesUtils.openSelectFilesPopup(accountId, hashes);
+						'Handler': function (accountId, hashes, attachments) {
+							const
+								Popups = require('%PathToCoreWebclientModule%/js/Popups.js'),
+								SelectFilesPopup = require('modules/%ModuleName%/js/popups/SelectFilesPopup.js')
+							;
+							Popups.showPopup(SelectFilesPopup, [attachments, accountId]);
 						}
 					});
 				});
--- js/popups/SelectFilesPopup.js
+++ js/popups/SelectFilesPopup.js
@@ -4,10 +4,14 @@ const
 	_ = require('underscore'),
 	ko = require('knockout'),
 
+	TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
 	Utils = require('%PathToCoreWebclientModule%/js/utils/Common.js'),
 
+	Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
 	Api = require('%PathToCoreWebclientModule%/js/Api.js'),
 	CAbstractPopup = require('%PathToCoreWebclientModule%/js/popups/CAbstractPopup.js'),
+	ModulesManager = require('%PathToCoreWebclientModule%/js/ModulesManager.js'),
+	Screens = require('%PathToCoreWebclientModule%/js/Screens.js'),
 
 	CFilesView = require('modules/FilesWebclient/js/views/CFilesView.js')
 ;
@@ -18,8 +22,6 @@ const
 function CSelectFilesPopup()
 {
 	CAbstractPopup.call(this);
-
-	this.callbackHandler = () => {};
 
 	this.filesView = new CFilesView(true, false);
 	this.filesView.onSelectClickPopupBound = () => {};
@@ -36,8 +38,18 @@ function CSelectFilesPopup()
 		}
 	}.bind(this.filesView);
 
+	this.isSaving = ko.observable(false);
+	this.isSaving.subscribe(function () {
+		this.filesView.disableRoute = this.isSaving();
+	}, this);
+
+	this.allowCreateFolder = ko.computed(function () {
+		return !this.isSaving();
+	}, this);
+	this.createFolderCommand = Utils.createCommand(this, this.createFolder, this.allowCreateFolder);
+
 	this.allowSelectFolder = ko.computed(function () {
-		return this.filesView.storageType() !== 'shared' || this.filesView.currentPath() !== '';
+		return !this.isSaving() && (this.filesView.storageType() !== 'shared' || this.filesView.currentPath() !== '');
 	}, this);
 	this.selectFolderCommand = Utils.createCommand(this, this.selectFolder, this.allowSelectFolder);
 }
@@ -47,11 +59,35 @@ _.extendOwn(CSelectFilesPopup.prototype, CAbstractPopup.prototype);
 CSelectFilesPopup.prototype.PopupTemplate = '%ModuleName%_SelectFilesPopup';
 
 /**
- * @param {function} callbackHandler
+ * @param {array} attachments
+ * @param {int} accountId
  */
-CSelectFilesPopup.prototype.onOpen = function (callbackHandler)
+CSelectFilesPopup.prototype.onOpen = function (attachments, accountId)
 {
-	this.callbackHandler = _.isFunction(callbackHandler) ? callbackHandler : () => {};
+	const attachmentsDataToUpload = [];
+	attachments.forEach(attach => {
+		const
+			fileData = {
+				FileName: attach.fileName(),
+				Size: attach.size(),
+				Hash: attach.hash(),
+				Type: attach.mimeType()
+			},
+			isFileSizeLessThanUploadLimit = this.filesView.isFileSizeLessThanUploadLimit(fileData)
+		;
+
+		if (isFileSizeLessThanUploadLimit) {
+			attachmentsDataToUpload.push(fileData);
+		}
+	});
+
+	if (attachmentsDataToUpload.length === 0) {
+		this.closePopup();
+		return;
+	}
+
+	this.attachmentsData = attachmentsDataToUpload;
+	this.accountId = accountId;
 
 	this.filesView.onShow();
 	this.filesView.routeFiles('personal', '');
@@ -70,8 +106,60 @@ CSelectFilesPopup.prototype.onBind = function ()
 
 CSelectFilesPopup.prototype.selectFolder = function ()
 {
-	this.callbackHandler(this.filesView.storageType(), this.filesView.currentPath());
-	this.closePopup();
+	const hashes = [];
+
+	this.attachmentsData.forEach(fileData => {
+		const isFileCanBeUploaded = this.filesView.isFileCanBeUploaded(fileData);
+		if (isFileCanBeUploaded) {
+			this.filesView.onFileUploadSelect(fileData.Hash, fileData);
+			this.filesView.onFileUploadStart(fileData.Hash);
+			this.filesView.onFileUploadProgress(fileData.Hash, fileData.Size * 0.2, fileData.Size);
+			hashes.push(fileData.Hash);
+		}
+	});
+
+	if (hashes.length > 0) {
+		this.saveToFolder(this.filesView.storageType(), this.filesView.currentPath(), this.accountId, hashes);
+	}
+};
+
+CSelectFilesPopup.prototype.saveToFolder = function(storage, path, accountId, hashes)
+{
+	Screens.showLoading(TextUtils.i18n('COREWEBCLIENT/INFO_LOADING'));
+	const
+		parameters = {
+			'AccountID': accountId,
+			'Attachments': hashes,
+			'Storage': storage,
+			'Path': path
+		},
+		responseHandler = response => {
+			this.isSaving(false);
+			Screens.hideLoading();
+			if (response.Result) {
+				const headerItemView = ModulesManager.run('FilesWebclient', 'getHeaderItem');
+				if (headerItemView && headerItemView.item) {
+					headerItemView.item.recivedAnim(true);
+				}
+				this.attachmentsData.forEach(fileData => {
+					this.filesView.onFileUploadComplete(fileData.Hash, true, { Result: true });
+				});
+				if (this.filesView.uploadingFiles().length === 0) {
+					const
+						filesCount = this.attachmentsData.length,
+						reportLang = '%MODULENAME%/REPORT_FILES_SAVED_SUCCESSFULLY_PLURAL',
+						reportText = TextUtils.i18n(reportLang, null, null, filesCount)
+					;
+					Screens.showReport(reportText);
+				}
+				setTimeout(() => { this.closePopup(); }, 1000);
+			} else {
+				Api.showErrorByCode(response);
+			}
+		}
+	;
+	this.isSaving(true);
+	Ajax.send('%ModuleName%', 'Save', parameters, responseHandler);
 };
 
 CSelectFilesPopup.prototype.createFolder = function ()
