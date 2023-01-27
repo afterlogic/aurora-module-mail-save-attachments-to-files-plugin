d9407b4b73a08b12b59dca91f94ffd0dc8adf419
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:58 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -16,73 +16,67 @@ namespace Aurora\Modules\MailSaveAttachmentsToFilesPlugin;
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	/* 
-	 * @var $getFilecacheManager()Manager \Aurora\System\Managers\Filecache 
-	 */	
-	public $oApiFilecacheManager = null;
+    /*
+     * @var $getFilecacheManager()Manager \Aurora\System\Managers\Filecache
+     */
+    public $oApiFilecacheManager = null;
 
-	public function getFilecacheManager()
-	{
-		if ($this->oApiFilecacheManager === null)
-		{
-			$this->oApiFilecacheManager = new \Aurora\System\Managers\Filecache();
-		}
+    public function getFilecacheManager()
+    {
+        if ($this->oApiFilecacheManager === null) {
+            $this->oApiFilecacheManager = new \Aurora\System\Managers\Filecache();
+        }
 
-		return $this->oApiFilecacheManager;
-	}
+        return $this->oApiFilecacheManager;
+    }
 
-	public function init() 
-	{
-	}	
-	
-	/**
-	 * 
-	 * @return boolean
-	 */
-	public function Save($UserId, $AccountID, $Attachments = [], $Storage = 'personal', $Path = '')
-	{
-		$mResult = false;
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
-		$oMailModuleDecorator = \Aurora\Modules\Mail\Module::Decorator();
-		if ($oMailModuleDecorator)
-		{
-			$aTempFiles = $oMailModuleDecorator->SaveAttachmentsAsTempFiles($AccountID, $Attachments);
-			if (\is_array($aTempFiles))
-			{
-				$sUUID = \Aurora\System\Api::getUserUUIDById($UserId);
-				foreach ($aTempFiles as $sTempName => $sData)
-				{
-					$aData = \Aurora\System\Api::DecodeKeyValues($sData);
-					if (\is_array($aData) && isset($aData['FileName']))
-					{
-						$sFileName = (string) $aData['FileName'];
-						$rResource = $this->getFilecacheManager()->getFile($sUUID, $sTempName);
-						if ($rResource)
-						{
-							$aArgs = array(
-								'UserId' => $UserId,
-								'Type' => $Storage,
-								'Path' => $Path,
-								'Name' => $sFileName,
-								'Data' => $rResource,
-								'Overwrite' => false,
-								'RangeType' => 0,
-								'Offset' => 0,
-								'ExtendedProps' => array()
-							);
-							\Aurora\System\Api::GetModuleManager()->broadcastEvent(
-								'Files',
-								'CreateFile', 
-								$aArgs,
-								$mResult
-							);							
-						}
-					}
-				}
-			}			
-		}
-		
-		return $mResult;
-	}
+    public function init()
+    {
+    }
+
+    /**
+     *
+     * @return boolean
+     */
+    public function Save($UserId, $AccountID, $Attachments = [], $Storage = 'personal', $Path = '')
+    {
+        $mResult = false;
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+
+        $oMailModuleDecorator = \Aurora\Modules\Mail\Module::Decorator();
+        if ($oMailModuleDecorator) {
+            $aTempFiles = $oMailModuleDecorator->SaveAttachmentsAsTempFiles($AccountID, $Attachments);
+            if (\is_array($aTempFiles)) {
+                $sUUID = \Aurora\System\Api::getUserUUIDById($UserId);
+                foreach ($aTempFiles as $sTempName => $sData) {
+                    $aData = \Aurora\System\Api::DecodeKeyValues($sData);
+                    if (\is_array($aData) && isset($aData['FileName'])) {
+                        $sFileName = (string) $aData['FileName'];
+                        $rResource = $this->getFilecacheManager()->getFile($sUUID, $sTempName);
+                        if ($rResource) {
+                            $aArgs = array(
+                                'UserId' => $UserId,
+                                'Type' => $Storage,
+                                'Path' => $Path,
+                                'Name' => $sFileName,
+                                'Data' => $rResource,
+                                'Overwrite' => false,
+                                'RangeType' => 0,
+                                'Offset' => 0,
+                                'ExtendedProps' => array()
+                            );
+                            \Aurora\System\Api::GetModuleManager()->broadcastEvent(
+                                'Files',
+                                'CreateFile',
+                                $aArgs,
+                                $mResult
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
+        return $mResult;
+    }
 }
